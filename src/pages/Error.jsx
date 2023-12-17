0becffa7dd2f8a79f12b0022c09531277fdff686
Lozor41 <pierre-8992@outlook.fr>
2023-12-17 14:23:38 +0100

    IW-87 Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,13 @@
-const Home = () => <div>404</div>
+import { Link } from 'react-router-dom'
+
+const Home = () => (
+    <div id="error">
+        <h1>404</h1>
+        <h3>Oups! La page que vous demandez n'existe pas.</h3>
+        <Link to="/" id="returnhome">
+            Retourner sur la page d'accueil
+        </Link>
+    </div>
+)
 
 export default Home
